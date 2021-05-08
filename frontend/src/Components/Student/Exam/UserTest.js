9753b7e3065d31cc79062ee3a27cc11346f306d8
Your Name <jontysaha600@gmail.com>
2021-05-09 01:26:51 +0530

    exam-Done

--- frontend/src/Components/Student/Exam/UserTest.js
+++ frontend/src/Components/Student/Exam/UserTest.js
@@ -15,7 +15,7 @@ class UserTest extends Component {
       name: "React",
       questions: [],
       details: [],
-      selectedOption: "",
+      selectedOption: [],
       answers: [],
     };
 
@@ -27,15 +27,61 @@ class UserTest extends Component {
     console.log(this.state.selectedOption);
   }
 
-  onValueChange(e) {
+  onValueChange(e, id) {
     // let answers = this.state.answers;
     // answers.push(e.target.value);
     // this.setState({ answers });
-    console.log("selected option", e.target.value);
-    this.setState({ selectedOption: e.target.value });
+    // console.log("selected option", e.target.value);
+    // console.log("id", id);
+    this.setState({ selectedOption: e.target.value, id: id });
+    console.log(this.state.answers);
+
+    this.state.answers[id] = e.target.value;
+    // this.setState({ answers: answers });
+    console.log(this.state.answers);
   }
 
-  handleOnChange(e) {}
+  onSubmit = () => {
+    console.log(this.state.answers);
+
+    axios
+      .post("http://localhost:9000/test_respons/31", {
+        answers: this.state.answers,
+        email: "sayak",
+      })
+      .then((response) => {
+        console.log(response);
+
+        // if (!response.data.errmsg) {
+        //   console.log("successful signup");
+        //   this.setState({
+        //     //redirect to login page
+        //     redirectTo: "/login",
+        //   });
+        // } else {
+        //   console.log("username already taken");
+        // }
+      });
+  };
+  // postDataHandler = () => {
+  //   const post = {
+  //     from: this.state.from,
+  //     to: this.state.to,
+  //     cause: this.state.cause,
+  //   };
+
+  //   this.setState({ message: "Application for leave given" });
+
+  //   const cookies = new Cookies();
+
+  //   axios.post(
+  //     `http://localhost:9000/leave/${cookies.get("userid")}/${cookies.get(
+  //       "username"
+  //     )}`,
+  //     post
+  //   );
+  // };
+
   componentDidMount() {
     const id = this.props.match.params.id;
     console.log(id);
@@ -46,21 +92,52 @@ class UserTest extends Component {
     });
   }
   render() {
-    const questions = this.state.questions.map((question) => {
-      console.log(this.state.answers);
+    // for (let i = 0; i < this.state.questions.length; i++) {
+    //   return (
+    //     <UserTestCard
+    //       questions={this.state.questions[i].question}
+    //       optionA={this.state.questions[i].options[0].A}
+    //       optionB={this.state.questions[i].options[0].B}
+    //       optionC={this.state.questions[i].options[0].C}
+    //       optionD={this.state.questions[i].options[0].D}
+    //       marks={this.state.questions[i].marks}
+    //       id={this.state.questions[i]._id}
+    //       onChange={(e) => this.onValueChange(e, i)}
+    //     />
+    //   );
+    // }
+    console.log(this.state.selectedOption);
+
+    const questions = this.state.questions.map((question, i) => {
       return (
         <UserTestCard
+          key={i}
           questions={question.question}
           optionA={question.options[0].A}
           optionB={question.options[0].B}
           optionC={question.options[0].C}
           optionD={question.options[0].D}
           marks={question.marks}
-          onChange={(e) => this.onValueChange(e)}
-          selected={this.state.selectedOption}
+          id={question._id}
+          onChange={(e) => this.onValueChange(e, i)}
         />
       );
     });
+    // const questions = this.state.questions.map((question) => {
+    //   console.log(this.state.answers);
+    //   return (
+    //     <UserTestCard
+    //       questions={question.question}
+    //       optionA={question.options[0].A}
+    //       optionB={question.options[0].B}
+    //       optionC={question.options[0].C}
+    //       optionD={question.options[0].D}
+    //       marks={question.marks}
+    //       onChange={(e) => this.onValueChange(e)}
+    //       selected={this.state.selectedOption}
+    //     />
+    //   );
+    // });
     return (
       <div>
         <Jumbotron className="test-details">
@@ -77,7 +154,13 @@ class UserTest extends Component {
           {questions}
           <Button>Submit</Button>
         </form> */}
-        {questions}
+
+        <form onSubmit={this.onSubmit}>
+          {questions}
+          <Button type="submit" value="submit">
+            Submit
+          </Button>
+        </form>
       </div>
     );
   }
--- frontend/src/Components/Student/Exam/UserTestCard.js
+++ frontend/src/Components/Student/Exam/UserTestCard.js
@@ -1,56 +1,51 @@
 import React, { Component } from "react";
 import { Card, Button, CardHeader, CardText, Row, Col } from "reactstrap";
 
-const UserTestCard = (props) => {
-  return (
-    <form>
-      <p>Question:{props.questions}</p>
-
-      <div className="radio">
+class UserTestCard extends Component {
+  onHandleChange = () => {};
+  render() {
+    return (
+      <form>
+        <p>Question:{this.props.questions}</p>
+        <div className="">A: {this.props.optionA}</div>
+        <div className="">B: {this.props.optionB}</div>
+        <div className="">C: {this.props.optionC}</div>
+        <div className="">D: {this.props.optionD}</div>
         <input
           type="radio"
-          value={props.optionA}
-          checked={props.selected === props.optionA}
-          onChange={props.onChange}
+          id="A"
+          name="option"
+          value="A"
+          onChange={this.props.onChange}
         />
-        A: {props.optionA}
-      </div>
-      <div className="radio">
-        <label>
-          <input
-            type="radio"
-            value={props.optionB}
-            checked={props.selected === props.optionB}
-            onChange={props.onChange}
-          />
-          B: {props.optionB}
-        </label>
-      </div>
-      <div className="radio">
-        <label>
-          <input
-            type="radio"
-            value={props.optionC}
-            checked={props.selected === props.optionC}
-            onChange={props.onChange}
-          />
-          C: {props.optionC}
-        </label>
-      </div>
-      <div className="radio">
-        <label>
-          <input
-            type="radio"
-            value={props.optionD}
-            checked={props.selected === props.optionD}
-            onChange={props.onChange}
-          />
-          D: {props.optionD}
-        </label>
-      </div>
-      <div>Selected option is : {props.selected}</div>
-    </form>
-  );
-};
+        <label for="A">A</label>
+        <input
+          type="radio"
+          id="B"
+          name="option"
+          value="B"
+          onChange={this.props.onChange}
+        />
+        <label for="B">B</label>
+        <input
+          type="radio"
+          id="C"
+          name="option"
+          value="C"
+          onChange={this.props.onChange}
+        />
+        <label for="C">C</label>
+        <input
+          type="radio"
+          id="D"
+          name="option"
+          value="D"
+          onChange={this.props.onChange}
+        />
+        <label for="D">D</label>
+      </form>
+    );
+  }
+}
 
 export default UserTestCard;
